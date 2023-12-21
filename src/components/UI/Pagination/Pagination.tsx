8fcc69dd90e863d6cf8ec6d5b586e211dff27278
AntonioDorosh <antondoroshenko92@gmail.com>
2023-12-21 22:06:35 +0200

    pagination

--- src/components/UI/Pagination/Pagination.tsx
+++ src/components/UI/Pagination/Pagination.tsx
@@ -8,17 +8,17 @@ type PaginationProps = {
 
 const Pagination = ({currentPage, onChangePage}: PaginationProps) => {
     return (
-            <StyledReactPaginate
-                gap={20}
-                marginPagesDisplayed={5}
-                breakLabel="..."
-                nextLabel=">"
-                previousLabel="<"
-                onPageChange={(event) => onChangePage(event.selected + 1)}
-                pageRangeDisplayed={5}
-                pageCount={5}
-                forcePage={currentPage - 1}
-            />
+        <StyledReactPaginate
+            gap={20}
+            marginPagesDisplayed={5}
+            breakLabel="..."
+            nextLabel=">"
+            previousLabel="<"
+            onPageChange={(event) => onChangePage(event.selected + 1)}
+            pageRangeDisplayed={5}
+            pageCount={5}
+            forcePage={currentPage - 1}
+        />
     );
 };
 
