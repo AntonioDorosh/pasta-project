f659e49174d633dd7ea1e4140311cd5e991ee5a7
AntonioDorosh <antondoroshenko92@gmail.com>
2024-01-22 18:21:01 +0200

    refactor(code): some fixes

---
+++ src/components/Cart/QuantityButtons.tsx
@@ -0,0 +1,23 @@
+import React, {FC} from 'react';
+import Flex from "../../styles/Flex/Flex.ts";
+import {px2vw, remCalc} from "../../utils";
+import {Button} from "../UI/Button/Button.tsx";
+import Text from "../../styles/Text/Text.ts";
+
+type QuantityControlProps = {
+    increase: () => void,
+    decrease: () => void,
+    quantity: number
+}
+
+export const QuantityButtons: FC<QuantityControlProps> = ({increase, decrease, quantity}) => (
+    <Flex alignItems={'center'} gap={px2vw(15)}>
+        <Button onClick={decrease}>
+            <img src='src/assets/minus.svg' alt="minus"/>
+        </Button>
+        <Text fontSize={remCalc(22)} fontWeight={700}>{quantity}</Text>
+        <Button onClick={increase}>
+            <img src='src/assets/plus.svg' alt="plus"/>
+        </Button>
+    </Flex>
+);
